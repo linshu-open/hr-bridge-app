1afa2fb6a46e71392b93a723db1c6b8690600428
linshu-open <qidizw@163.com>
2026-04-19 10:54:44 +0800

    v2.0: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
         // 版本信息注入（BuildConfig 可用）
         buildConfigField("String", "DEFAULT_SERVER_URL", "\"http://100.126.107.40:18890/jarvis/sensor/heart-rate\"")
         buildConfigField("String", "DEFAULT_BATCH_URL",  "\"http://100.126.107.40:18890/jarvis/sensor/heart-rate/batch\"")
+        buildConfigField("String", "DEFAULT_SENSOR_BASE_URL", "\"http://100.126.107.40:18890/jarvis/sensor\"")
         buildConfigField("String", "GITHUB_RELEASES_API","\"https://api.github.com/repos/linshu-open/hr-bridge-app/releases/latest\"")
     }
 
