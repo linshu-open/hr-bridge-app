8f04b2831d5418974014b67fff84d4a4ebc90203
linshu-open <linshu-open@users.noreply.github.com>
2026-05-17 14:50:21 +0800

    fix(android): import generated R in keepalive service

--- app/src/main/java/cn/jarvis/hrbridge/service/KeepAliveService.kt
+++ app/src/main/java/cn/jarvis/hrbridge/service/KeepAliveService.kt
@@ -11,6 +11,7 @@ import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import cn.jarvis.hrbridge.MainActivity
+import cn.jarvis.hrbridge.R
 import cn.jarvis.hrbridge.util.Logger
 
 /**
